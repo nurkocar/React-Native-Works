5181b04d31429c3df26c18afaa7aa11889cb9ca2
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-29 11:52:13 +0000

    added react redux inClass src and index files

---
+++ 27_10_20_REACT_REDUX/src/pages/index.js
@@ -0,0 +1,4 @@
+import { forModalPresentationIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
+
+export * from './Component_A';
+export * from './Component_B';
