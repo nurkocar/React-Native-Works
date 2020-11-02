dced9291d9bbedcd5c79c9181c1f9db6deb9937f
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-11-02 15:10:59 +0000

    updated Redux2 with FavoriteItem and RemoveFromFavorite action on reducer

--- 31_10_20_REACT_REDUX_2/src/context/reducer.js
+++ 31_10_20_REACT_REDUX_2/src/context/reducer.js
@@ -1,16 +1,32 @@
 export function reducer(state, action) {
     switch (action.type) {
         case "ADD_TO_FAVORITE":
-            // const newList = [...state.favoriteList];
-            // newList.push(action.payload.selectedRestaurant);
-            // state.favoriteList = newList;
-            // return { ...state }
-
-            return {
-                ...state,
-                favoriteList: [...state.favoriteList, action.payload.selectedRestaurant]
+            const newList = [...state.favoriteList];
+            if (newList.indexOf(action.payload.selectedRestaurant) > -1){
+                newList
+            }else{
+                newList.push(action.payload.selectedRestaurant);
             }
+            state.favoriteList = newList;
+            return { ...state }
 
+             //push methodu yeni olusan arrayin length ini doner. Dolayisiyla tek satirda push yapilan array 
+            //birseye atanirsa arrayi kaybederiz, number olur return degeri. Eger state i neden direkt
+            // donmuyoruz diyorsaniz baska yerde baska reducerlar ile state degistiriliyor olabilir. Bu da
+            // application state ini bozar. Dolayisiyla state direkt degistirilmez, kopyasi alinir o degistirilir
+            
+
+            // return {
+            //     ...state,
+            //     favoriteList: [...state.favoriteList, action.payload.selectedRestaurant]
+            // }
+        case "REMOVE_FROM_FAVORİTE":
+            const updateList = [...state.favoriteList]
+            const index = updateList.indexOf(action.payload.selectedRestaurant);
+            if (index > -1) {
+                updateList.splice(index, 1)};
+            state.favoriteList = updateList;
+            return {...state}
         default:
             return state;
     }
--- 31_10_20_REACT_REDUX_2/src/pages/Favorites.js
+++ 31_10_20_REACT_REDUX_2/src/pages/Favorites.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { SafeAreaView, View, Text, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { SafeAreaView, View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { FavoriteItem } from "../components"
 
 const Favorites = (props) => {
     const favList = useSelector(state => state.favoriteList);
+    const dispatch = useDispatch()
+
+    const renderFav = ({item}) =>{
+            return (
+                <FavoriteItem
+                    item={item}
+                    onRemoveFavorite={() => dispatch({
+                        type:  "REMOVE_FROM_FAVORİTE",
+                        payload: { selectedRestaurant: item }
+                    })}
+                />
+            )
+        }
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -11,7 +26,7 @@ const Favorites = (props) => {
                 <FlatList
                     keyExtractor={(_, index) => index.toString()}
                     data={favList}
-                    renderItem={({ item }) => <Text>{item.name}</Text>}
+                    renderItem={renderFav}
                     ListEmptyComponent={() => <Text>Nothing on fav..</Text>}
                 />
             </View>
@@ -20,3 +35,17 @@ const Favorites = (props) => {
 }
 
 export { Favorites };
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    text: {
+        marginLeft: 10,
+        fontWeight: '200'
+    }
+})
